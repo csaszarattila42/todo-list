d75f09a8144ec334fbfe3773bded7e0dcf726605
csaszarattila42 <csaszarattila90@gmail.com>
2022-05-17 13:34:34 +0200

    feat: Implement top App component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,39 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const LISTS = [
+  {
+    title: 'Home',
+    listItems: [
+      {
+        name: 'Clean up bottles and cans',
+        priority: 1,
+        deadLine: null,
+        done: false
+      },
+      {
+        name: 'Replace the phone of HELl',
+        priority: 0,
+        deadLine: new Date('2022-05-19'),
+        done: false
+      },
+    ]
+  },
+  {
+    title: 'Codecool',
+    listItems: [
+      {
+        name: 'Finish pet project',
+        priority: 4,
+        deadLine: new Date('2022-05-19'),
+        done: false
+      }
+    ]
+  }
+];
+
+
 ReactDOM.render(
-  <App />,
+  <App lists={LISTS}/>,
   document.getElementById('root')
 );
